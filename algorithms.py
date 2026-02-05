4d3ec50c525abc752d9903d33345fcddfa0fdb5a
Oleh <olegfostij@gmail.com>
2026-02-05 23:25:25 +0100

    Add custom sorting and searching algorithms

---
+++ algorithms.py
@@ -0,0 +1,14 @@
+def bubble_sort(arr):
+    arr = arr.copy()
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+def linear_search(arr, target):
+    for x in arr:
+        if x == target:
+            return True
+    return False
